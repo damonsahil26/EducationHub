355dbac1d2e85ca2dafe2f96ecb9b02005be7fb9
Sahil Malhotra <sahil061626@gmail.com>
2022-02-01 22:26:52 +0530

    Added scaffold and updated register data

--- EduHub/Data/Migrations/20220122113310_updated_user_table.Designer.cs
+++ EduHub/Data/Migrations/20220122113310_updated_user_table.Designer.cs
@@ -4,14 +4,16 @@ using EduHub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EduHub.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220122113310_updated_user_table")]
+    partial class updated_user_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
